31bfbb0c9275c131023f2b85496efbad35e9e024
piladzisi <piladzisi@gmail.com>
2021-01-05 16:58:17 +0200

    display users with city

---
+++ UsersList/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  UsersList
+//
+//  Created by Anna Sibirtseva on 05/01/2021.
+//
+
+import Foundation
+
+struct User {
+    var name: String
+    var address: [String : Any]
+    var city: String
+}
--- UsersList/UsersVC.swift
+++ UsersList/UsersVC.swift
@@ -9,24 +9,68 @@ import UIKit
 
 class UsersVC: UITableViewController {
     
-    let tempUsers = ["A", "B", "C", "D", "F"]
-
+    var users = [User]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hello world")
+        getUsers()
     }
 
     // MARK: - Table view data source
 
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        tempUsers.count
+        users.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        let user = tempUsers[indexPath.row]
-        cell.textLabel?.text = user
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+        let user = users[indexPath.row]
+        cell.textLabel?.text = user.name
+        cell.detailTextLabel?.text = user.city
         return cell
+    }
+}
+
+//MARK:  - Networking
+    
+extension UsersVC {
+    func getUsers() {
+        let baseUrl = "https://jsonplaceholder.typicode.com/"
+        guard let usersUrl = URL(string: (baseUrl + "users")) else { return }
+        
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: usersUrl) { (data, response, error) in
+            if let response = response  as? HTTPURLResponse{
+                print(response.statusCode)
+            }
+            
+            if let error = error {
+                print(error)
+            } else if let data = data {
+                do {
+                    guard let json = try JSONSerialization.jsonObject(with: data, options: []) as? [[String : Any]] else { return }
+                    var users = [User]()
+                    for user in json {
+                        guard
+                            let name = user["name"] as? String,
+                            let address = user["address"] as? [String : Any],
+                            let city = address["city"] as? String
+                        else { return }
+                        
+                        let user = User(name: name, address: address, city: city)
+                        users.append(user)
+                    }
+                    self.users = users
+                    //UI element tableView must be reloaded on main thread, while data fetching was sent on background thread
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
+                } catch {
+                    print(error)
+                }
+            }
+        }
+        
+        dataTask.resume()
     }
 }
